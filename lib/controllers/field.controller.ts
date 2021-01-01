0a938629172214a5d0a5ef53ded0376ae8b7d26a
Elry <rafaelgarcialima@outlook.com>
2021-01-01 01:50:59 -0300

    uri parameters with proper variable and types on each respective controller

--- lib/controllers/field.controller.ts
+++ lib/controllers/field.controller.ts
@@ -3,5 +3,9 @@ import { Request, Response } from "express";
 
 // unutilized field availability
 export const unutilized_field_availability = function(req:Request, res:Response) {
-  res.send('NOT IMPLEMENTED: Author list');
+  let fieldId:string = req.params.fieldId;
+  let endData:string = req.params.endDate;
+  let startDate:string = req.params.startDate;
+
+  res.send('NOT IMPLEMENTED: Field availability list');
 };
--- lib/controllers/player.controller.ts
+++ lib/controllers/player.controller.ts
@@ -3,10 +3,15 @@ import { Request, Response } from "express";
 
 // players who haven't played since specific date
 export const player_last_game = function(req:Request, res:Response) {
+  let date:string = req.params.date;
+
   res.send(`NOT IMPLEMENTED: Player last game ${req.params.date}`);
 };
 
 // Average review above 5 stars
 export const player_game_review_bFive = function(req:Request, res:Response) {
+  let numGames:string = req.params.numGames;
+  let avgRating:string = req.params.avgRating;
+
   res.send('NOT IMPLEMENTED: Player rating: ' + req.params.id);
 };
