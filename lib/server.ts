5582ea59a7c3bf2dfcd55caeb9280d2851d9b266
Elry <rafaelgarcialima@outlook.com>
2021-01-01 20:33:48 -0300

    Proper mongodb connection parameters

--- lib/server.ts
+++ lib/server.ts
@@ -27,7 +27,7 @@ const enableCORS = function (req, res, next) {
   next();
 };
 
-mongoose.connect(process.env.MONGO_URI, {})
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
 // home
 app.get('/', (req, res) => res.send('Celebreak challenge'));
