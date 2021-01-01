8517799fadc3703c1ba63ee7558c6f6edfdf716b
Elry <rafaelgarcialima@outlook.com>
2021-01-01 20:33:58 -0300

    controller test added

--- test/part2.spec.js
+++ test/part2.spec.js
@@ -1,7 +1,26 @@
 var assert = require('assert');
 
-describe("POST endpoint", () => {
-  it("should return something", (done) => {
+describe("players", () => {
+  it("should return lastgame", (done) => {
+    let result = [];
+    let currentDate = new Date('2020-01');
+
+    let players = [
+      {id: '1', name:'a', lastgame:new Date('2020-01')},
+      {id: '2', name:'b', lastgame:new Date('2020-01')},
+      {id: '3', name:'c', lastgame:new Date('2020-02')},
+      {id: '4', name:'d', lastgame:new Date('2020-03')},
+      {id: '5', name:'e', lastgame:new Date('2020-03')},
+    ];
+
+    Object.entries(players).forEach(([key, value]) => {
+      if(value.lastgame > currentDate){
+        result.push(value.id);
+      }
+    });
+
+    console.log(result);
+
     assert.strictEqual(1, 1);
     done();
   });
