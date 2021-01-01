f2f918b041de29223d610da316cc6fe683cee72d
Elry <rafaelgarcialima@outlook.com>
2021-01-01 01:51:15 -0300

    more readable uri parameter name

--- lib/routes/player.route.ts
+++ lib/routes/player.route.ts
@@ -5,6 +5,6 @@ const router:Router = Router();
 import { player_last_game, player_game_review_bFive} from "../controllers/player.controller";
 
 router.get('/last-game/:date', player_last_game);
-router.get('/review-below-five/:avg/:num', player_game_review_bFive);
+router.get('/review-below-five/:avgRating/:numGames', player_game_review_bFive);
 
 export default router;
