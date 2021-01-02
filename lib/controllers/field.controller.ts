d9e6826927cd2b3c5113de352aa8b0b4cfa1c6df
Elry <rafaelgarcialima@outlook.com>
2021-01-02 18:18:05 -0300

    func3 logic for search in a date range finished

--- lib/controllers/field.controller.ts
+++ lib/controllers/field.controller.ts
@@ -1,26 +1,25 @@
 import Field from "../models/field.model";
 import { Request, Response } from "express";
-import FieldAvailability from "lib/models/field_availability.model";
+import fieldAvailabilities from "../models/field_availability.model";
 
 // unutilized field availability
-export const unutilized_field_availability = function(req:Request, res:Response) {
+export const unutilized_field_availability = async function(req:Request, res:Response) {
   let count:number = 0;
   let status:number = 200;
   let fieldId:string = req.params.fieldId;
-  let endDate:Date = new Date(req.params.endDate);
-  let startDate:Date = new Date(req.params.startDate);
+  let endDate:string = req.params.endDate;
+  let startDate:string = req.params.startDate;
+  let searchParams:object = {"fieldId": fieldId, time:{ $gte: startDate, $lte: endDate }};
 
   try{
-    FieldAvailability.findById({fieldId:fieldId}, function(err, fieldAv){
-      if(fieldAv){
-        console.log(fieldAv);
-        fieldAv.array.forEach(element => {
-          let availabilityTime = new Date(element.time);
-          (availabilityTime > startDate && availabilityTime < endDate) ? count++ : 0;  
-        });
-      }else{ status = 204; }
+    await fieldAvailabilities.find(searchParams, function(err, result) {
+      if (err) {
+        status = 500;
+        console.log(err);
+      } else { count++; }
     });
-    
+    if(!count){ status = 204; }
+
     res.status(status).json(count);
   }catch(err){
     res.status(500).send(err);
