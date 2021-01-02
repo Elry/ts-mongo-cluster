21b01f42d354de990247991466ada275d7077503
Elry <rafaelgarcialima@outlook.com>
2021-01-01 23:17:02 -0300

    Added logic to func3

--- lib/controllers/field.controller.ts
+++ lib/controllers/field.controller.ts
@@ -1,11 +1,28 @@
 import Field from "../models/field.model";
 import { Request, Response } from "express";
+import FieldAvailability from "lib/models/field_availability.model";
 
 // unutilized field availability
 export const unutilized_field_availability = function(req:Request, res:Response) {
+  let count:number = 0;
+  let status:number = 200;
   let fieldId:string = req.params.fieldId;
-  let endData:string = req.params.endDate;
-  let startDate:string = req.params.startDate;
+  let endDate:Date = new Date(req.params.endDate);
+  let startDate:Date = new Date(req.params.startDate);
 
-  res.send('NOT IMPLEMENTED: Field availability list');
+  try{
+    FieldAvailability.findById({fieldId:fieldId}, function(err, fieldAv){
+      if(fieldAv){
+        console.log(fieldAv);
+        fieldAv.array.forEach(element => {
+          let availabilityTime = new Date(element.time);
+          (availabilityTime > startDate && availabilityTime < endDate) ? count++ : 0;  
+        });
+      }else{ status = 204; }
+    });
+    
+    res.status(status).json(count);
+  }catch(err){
+    res.status(500).send(err);
+  }
 };
