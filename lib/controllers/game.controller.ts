c223b10adf0a5fc9a7bf9a3433bc00ec5a3ff64a
Elry <rafaelgarcialima@outlook.com>
2021-01-03 20:14:28 -0300

    func2 weird solution

--- lib/controllers/game.controller.ts
+++ lib/controllers/game.controller.ts
@@ -1,32 +1,57 @@
 import { Request, Response } from "express";
-import Players from "../models/player.model";
 import GameReview from "../models/game_review.model";
+import Players, { IPlayer } from "../models/player.model";
 
 export const game_review_below_five = function(req:Request, res:Response):void {
   let status:number = 200;
-  let avgRating:string = req.params.avgRating;
+  let final:IPlayer[] = [];
   let numGames:number = Number(req.params.numGames);
+  let avgRating:number = Number(req.params.avgRating);
+  
+  /*
+  GameReview.aggregate([
+    { $group: { _id: {player: "$playerId", stars: "$stars"}}},
+    {$group: { _id: "$playerId", "avgStar": { "$avg": "$stars" }}}
+  ]).then(function(result){
+      result.map(ele=>{
+        console.log("loop", ele);
+      });
+      if(avgRating >= res.avgStar){
+        console.log("as expected", res);
+      }
+    res.status(status).json(result);
+  })
+  .catch(function(err){
+    console.log(err);
+    res.status(500).json(err);
+  });
+  */ 
 
   Players.find({})
-    .then(function(players){
-      players.forEach(player => {
-        GameReview.find({"playerId":player.id}).limit(numGames)
+    .then(async function(players){
+      Promise.all(players.map(async player => {
+        let playerid = await GameReview.find({"playerId":player.id}).limit(numGames)
           .then(function(reviews){
+            if(numGames > reviews.length){  numGames = reviews.length; }
             let holder:number = 0;
+
             reviews.forEach(review => {
               holder += Number(review.stars);
             });
 
-            console.log(holder/numGames);
-          })
-          .catch(function(err){
-            console.log(err);
+            if(avgRating >= (holder/numGames)){
+              return player.id;
+            }
           });
-      });
-      res.status(status).json(players);
+        final.push(playerid);
+      }))
+      .then(() => {
+        res.status(status).json(final);
+      })
+      .catch(function(err){ throw err; });
     })
     .catch(function(err){
       console.log(err);
       res.status(500).json(err);
-    });
+    });  
 }
