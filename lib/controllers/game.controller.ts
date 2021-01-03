31030a60129046a05d8c5a28db5de95ce1224784
Elry <rafaelgarcialima@outlook.com>
2021-01-03 02:38:50 -0300

    Almost finished func3

--- lib/controllers/game.controller.ts
+++ lib/controllers/game.controller.ts
@@ -1,10 +1,32 @@
 import { Request, Response } from "express";
+import Players from "../models/player.model";
 import GameReview from "../models/game_review.model";
 
-export const game_review_below_five = function(req:Request, res:Response) {
-  let numGames:string = req.params.numGames;
+export const game_review_below_five = function(req:Request, res:Response):void {
+  let status:number = 200;
   let avgRating:string = req.params.avgRating;
+  let numGames:number = Number(req.params.numGames);
 
-// 5ff1107c769d343a2ccbcc3f
-// 5ff0fa18769d343a2ccbcc3d
+  Players.find({})
+    .then(function(players){
+      players.forEach(player => {
+        GameReview.find({"playerId":player.id}).limit(numGames)
+          .then(function(reviews){
+            let holder:number = 0;
+            reviews.forEach(review => {
+              holder += Number(review.stars);
+            });
+
+            console.log(holder/numGames);
+          })
+          .catch(function(err){
+            console.log(err);
+          });
+      });
+      res.status(status).json(players);
+    })
+    .catch(function(err){
+      console.log(err);
+      res.status(500).json(err);
+    });
 }
